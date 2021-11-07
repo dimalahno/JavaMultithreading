b670e8cca15f102bbf7fdc198a13d90b3e6c4f31
dimalahno <dimalahno.rambler.ru>
2021-11-07 16:49:29 +0600

    Investigate different cases to create Threads.

---
+++ src/demo3/App.java
@@ -0,0 +1,19 @@
+package demo3;
+
+public class App {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("Hello " + i);
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+        t1.start();
+    }
+}
