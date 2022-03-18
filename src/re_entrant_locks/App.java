41468411e4d81e5973b34132acb8339b3370d672
dimalahno <dimalahno.rambler.ru>
2022-03-18 16:42:58 +0600

    Investigate re-entrant lock.

---
+++ src/re_entrant_locks/App.java
@@ -0,0 +1,33 @@
+package re_entrant_locks;
+
+public class App {
+
+    public static void main(String[] args) throws InterruptedException {
+        final var runner = new Runner();
+
+        Thread t1 = new Thread(() -> {
+            try {
+                runner.firstThread();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            try {
+                runner.secondThread();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        runner.finished();
+    }
+
+}
